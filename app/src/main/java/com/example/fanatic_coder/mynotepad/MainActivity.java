818afc87daaab7827b2efd08daac9e829d6e8f7f
Fanatic_Coder <GXenakis2@gmail.com>
2018-10-03 03:40:59 +0300

    Changed write_text edittext from orange, to white, and added 2 black lines.

--- app/src/main/java/com/example/fanatic_coder/mynotepad/MainActivity.java
+++ app/src/main/java/com/example/fanatic_coder/mynotepad/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 // import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-// import android.view.View;
+//import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
