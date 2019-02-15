763821d61845504210a90eaada5b7096fcb9532b
Fanatic_Coder <GXenakis2@gmail.com>
2019-02-15 19:38:53 +0200

    Added 3 queries and methods in NotesDAO.java and added table, primary key and columns.

--- app/src/main/java/com/example/fanatic_coder/mynotepad/db/NotesDAO.java
+++ app/src/main/java/com/example/fanatic_coder/mynotepad/db/NotesDAO.java
@@ -5,9 +5,12 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
 import com.example.fanatic_coder.mynotepad.model.Note;
+
+import java.util.ArrayList;
 
 @Dao
 public interface NotesDAO {
@@ -20,4 +23,14 @@ public interface NotesDAO {
 
     @Update
     void updateNote(Note note);
+
+    @Query("SELECT * FROM notes") // Show all notes from database
+    ArrayList<Note> getNotes();
+
+    @Query("SELECT * FROM notes WHERE id = :noteId") // Get the notes by their id
+    Note getNoteById(int noteId);
+
+    @Query("DELETE FROM notes WHERE id = :noteId") // Delete notes by their id
+    void deleteNoteById(int noteId);
+
 }
--- app/src/main/java/com/example/fanatic_coder/mynotepad/model/Note.java
+++ app/src/main/java/com/example/fanatic_coder/mynotepad/model/Note.java
@@ -1,8 +1,22 @@
 package com.example.fanatic_coder.mynotepad.model;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "notes")
 public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name = "text")
     private String writeText;
+    @ColumnInfo(name = "date")
     private long noteDate;
+
+    public Note(String writeText, long noteDate) {
+        this.writeText = writeText;
+        this.noteDate = noteDate;
+    }
 
     public String getWriteText() {
         return writeText;
@@ -20,8 +34,11 @@ public class Note {
         this.noteDate = noteDate;
     }
 
-    public Note(String writeText, long noteDate) {
-        this.writeText = writeText;
-        this.noteDate = noteDate;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
