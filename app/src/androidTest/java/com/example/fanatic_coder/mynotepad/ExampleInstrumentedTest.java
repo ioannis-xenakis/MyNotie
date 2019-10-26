aa2e2c7dcca3533519b6ee2ef31a8f096059aa59
Fanatic_Coder <GXenakis2@gmail.com>
2019-10-27 02:45:38 +0300

    Replaced 1 dependency in build.gradle, because of AndroidJUnit4 line deprecated in ExampleInstrumentedTest.java and added instrumented test code in ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/fanatic_coder/mynotepad/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fanatic_coder/mynotepad/ExampleInstrumentedTest.java
@@ -2,11 +2,17 @@ package com.example.fanatic_coder.mynotepad;
 
 import android.content.Context;
 import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +22,31 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.fanatic_coder.mynotepad", appContext.getPackageName());
+    }
+
+    @Test
+    public void userEntersNoteTitle() {
+        onView(withId(R.id.noteTitle)).perform(typeText("Shopping List"));
+    }
+
+    @Test
+    public void userEntersBodyText() {
+        onView(withId(R.id.noteBodyText)).perform(typeText("Tomatoes Potatoes Cucumbers Potato Chips Bananas"));
+    }
+
+    @Test
+    public void userEntersBodyTextAndNoteTitle() throws InterruptedException {
+        onView(withId(R.id.noteTitle)).perform(typeText("Hello Note")).perform(closeSoftKeyboard());
+        Thread.sleep(250);
+        onView(withId(R.id.noteBodyText)).perform(typeText("Hey Dude"));
     }
 }
