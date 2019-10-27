b93270def641e3716ce0ff108485d0319f0bdc13
Fanatic_Coder <GXenakis2@gmail.com>
2019-10-27 03:22:07 +0300

    Added one more instrumented test method for seeing if clear text button works.

--- app/src/androidTest/java/com/example/fanatic_coder/mynotepad/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fanatic_coder/mynotepad/ExampleInstrumentedTest.java
@@ -10,9 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -48,5 +51,17 @@ public class ExampleInstrumentedTest {
         onView(withId(R.id.noteTitle)).perform(typeText("Hello Note")).perform(closeSoftKeyboard());
         Thread.sleep(250);
         onView(withId(R.id.noteBodyText)).perform(typeText("Hey Dude"));
+    }
+
+    @Test
+    public void checkingIfClearTextButtonWorks() throws InterruptedException {
+        onView(withId(R.id.noteTitle)).perform(typeText("Lorem Ipsum")).perform(closeSoftKeyboard());
+        Thread.sleep(250);
+        onView(withId(R.id.noteBodyText)).perform(typeText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rhoncus tristique libero, in scelerisque nulla fermentum nec. Nam vel auctor enim. Donec eu purus molestie, fermentum libero eget, convallis purus. In eget fermentum ligula, nec porttitor tortor. Nullam blandit sapien nec ex sodales aliquet. Nunc massa urna, pharetra quis blandit at, vestibulum quis neque. Sed efficitur arcu tellus, a suscipit metus congue id. Duis interdum arcu sit amet suscipit scelerisque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;\n" +
+                "\n" +
+                "Morbi eu tortor a nisi ultricies fermentum. Sed eu egestas justo, a vestibulum arcu. Mauris vitae lorem nibh. Proin ultrices ex sit amet mauris porttitor, varius molestie neque venenatis. In eleifend, massa in gravida mattis, neque dui egestas neque, ut dapibus orci tellus sit amet justo. Suspendisse tincidunt volutpat nulla, id vestibulum erat. Sed id mauris metus. Ut faucibus cursus magna, convallis imperdiet tortor tristique a. Mauris sapien odio, elementum vulputate mauris sed, feugiat vestibulum augue. Morbi volutpat nisl in est facilisis tristique sit amet ac augue. Vivamus non justo in felis malesuada porta. Vestibulum interdum varius semper. Nam lobortis iaculis augue, id semper tellus congue et. Nunc tincidunt euismod arcu a volutpat. Maecenas vel dui at nisl blandit malesuada ut a sem.")).perform(closeSoftKeyboard());
+        Thread.sleep(250);
+        onView(withId(R.id.clearText)).perform(click());
+        onView(withId(R.id.noteTitle)).check(matches(withText("")));
     }
 }
