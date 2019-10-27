47703fe25cbd739c65a8babdac810e7febc9ddd6
Fanatic_Coder <GXenakis2@gmail.com>
2019-10-27 03:01:17 +0200

    Fixed deprecated code, in Instrumented Tests.

--- app/src/androidTest/java/com/example/fanatic_coder/mynotepad/InstrumentedTests.java
+++ app/src/androidTest/java/com/example/fanatic_coder/mynotepad/InstrumentedTests.java
@@ -1,8 +1,9 @@
 package com.example.fanatic_coder.mynotepad;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -31,7 +32,7 @@ public class InstrumentedTests {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.fanatic_coder.mynotepad", appContext.getPackageName());
     }
