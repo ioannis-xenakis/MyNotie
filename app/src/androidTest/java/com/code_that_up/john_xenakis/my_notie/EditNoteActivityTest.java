a711d7ed5383f55dcb0d93a9264dd293d846f316
Ioannis-Xenakis <Xenakis.I.Contact@gmail.com>
2021-05-24 22:32:05 +0300

    Completed adding license section, to source files.
    Added license section, to NotesAdapter.java file.
    
    Added license section, to MoreMenuButtonListener.java file.
    
    Added license section, to NoteEventListener.java file.
    
    Added license section, to NotesDAO.java file.
    
    Added license section, to NotesDB.java file.
    
    Added license section, to Note.java file.
    
    Added license section, to NoteUtils.java file.
    
    Added license section, to OtherUtils.java file.
    
    Added license section, to SpacesItemGrid.java file.
    
    Added license section, to EditNoteActivity.java file.
    
    Added license section, to MyNotesActivity.java file.
    
    Added license section, to EditNoteActivityTest.java file.
    
    Added license section, to MyNotesActivityTest.java file.
    
    Added license section, to TestUtils.java file.
    
    Added license section, to ic_launcher_background.xml file.
    
    Added license section, to ic_launcher_foreground.xml file.
    
    Added license section, to popup_menu_background.xml file.
    
    Added license section, to activity_edit_note.xml file.
    
    Added license section, to activity_edit_note.xml (sw600dp) file.
    
    Added license section, to activity_my_notes.xml file.
    
    Added license section, to activity_my_notes.xml (land) file.
    
    Added license section, to activity_my_notes.xml (sw600dp) file.
    
    Added license section, to nav_header_layout.xml file.
    
    Added license section, to note_layout.xml file.
    
    Added license section, to menu_bottom_top_bar.xml file.
    
    Added license section, to menu_nav_view.xml file.
    
    Added license section, to menu_note_more.xml file.
    
    Added license section, to menu_top_bar_edit_note.xml file.
    
    Added license section, to menu_top_bar_select_notes.xml file.
    
    Added license section, to ic_launcher.xml (anydpi-v26) file.
    
    Added license section, to ic_launcher_round.xml (anydpi-v26) file.
    
    Added license section, to colors.xml file.
    
    Added license section, to dimens.xml file.
    
    Added license section, to strings.xml file.
    
    Added license section, to styles.xml file.

--- app/src/androidTest/java/com/code_that_up/john_xenakis/my_notie/EditNoteActivityTest.java
+++ app/src/androidTest/java/com/code_that_up/john_xenakis/my_notie/EditNoteActivityTest.java
@@ -28,6 +28,25 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+/*
+    My Notie is a note taking app, write notes and save them to see them and remember later.
+    Copyright (C) 2021  Ioannis Xenakis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Anything you want to contact me for, contact me with an e-mail, at: Xenakis.i.Contact@gmail.com
+    I'll be happy to help you, or discuss anything with you! */
 
 /**
  * <h2>EditNoteActivityTest</h2> is a class, dedicated for instrumented tests on EditNoteActivity,
