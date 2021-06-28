184e948c8cfb3f166fbf7cad9f835224b7502874
Ioannis-Xenakis <Xenakis.I.Contact@gmail.com>
2021-06-28 18:44:32 +0300

    Created empty Add Or Manage Folders Activity.

---
+++ app/src/main/java/com/code_that_up/john_xenakis/my_notie/AddOrManageFoldersActivity.java
@@ -0,0 +1,14 @@
+package com.code_that_up.john_xenakis.my_notie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddOrManageFoldersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_or_manage_folders);
+    }
+}
